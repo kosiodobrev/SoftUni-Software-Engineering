916aed22e631c1592b02200f903cf397dafdb81b
kosiodobrev <kosiodobrev@gmail.com>
2023-10-10 15:41:59 +0200

    Commit SoftUni exercises

---
+++ 2. Python-Fundamentals/5.1 Functions_Exercise/10. perfect_number.py
@@ -0,0 +1,14 @@
+def is_perfect(some_number: int) -> str:
+    divisors_sum = 0
+    for divisor in range(1, some_number):
+        if some_number % divisor == 0:
+            divisors_sum += divisor
+    if some_number == divisors_sum:
+        return "We have a perfect number!"
+    else:
+        return "It's not so perfect."
+
+
+
+number = int(input())
+print(is_perfect(number))
