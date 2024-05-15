df4e54afa4ac46e8f79f9a7a37124107521162aa
kosiodobrev <kosiodobrev@gmail.com>
2024-05-15 18:03:06 +0200

    1

--- 3. Python-Advanced/2. Lists_as_Stacks_and_Queues_Exercise/8. crossroads.py
+++ 3. Python-Advanced/2. Lists_as_Stacks_and_Queues_Exercise/8. crossroads.py
@@ -1,31 +1,10 @@
-from collections import deque
-
 green_light = int(input())
-free_window = int(input())
+free_windows = int(input())
 command = input()
-all_passed = True
-current_car = deque()
-passed_cars = 0
+green_light_cycle = False
 
+while command != "END":
+    if
 
-while command != "End":
-    if command == "green":
-        green_light_copy, cycle_copy = green_light, free_window
-        while green_light_copy:
-            if current_car:
-
-        current_green_light = green_light
-    else:
-        current_car = deque(command)
-
-    for i in range(green_light):
-        if current_car:
-            current_car.popleft()
-            current_green_light -= 1
-        else:
-            break
 
     command = input()
-
-
-
