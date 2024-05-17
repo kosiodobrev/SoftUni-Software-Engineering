9c839469075b47e749ffb460f29b914bc8296a0e
kosiodobrev <kosiodobrev@gmail.com>
2024-05-17 11:58:58 +0200

    1

---
+++ 3. Python-Advanced/3. Tuples_and_Sets/4.2 parking_lot.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+parking = set()
+
+for _ in range(n):
+    direction, car_number = input().split(", ")
+
+    if direction == "IN":
+        parking.add(car_number)
+    elif direction == "OUT":
+        if car_number in parking:
+            parking.remove(car_number)
+
+
+if parking:
+    print(*parking, sep="\n")
+else:
+    print(f"Parking Lot is Empty")
